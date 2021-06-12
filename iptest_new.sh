93a82b838e28be120b4d7493908bd468742aea19
sistelca <sistelca@hotmail.com>
2021-06-12 10:06:28 -0400

    act ip

---
+++ iptest_new.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+/sbin/ip a l enp0s7 | grep "inet " > /home/luis/cibercom/midir/ip.txt
+cd /home/luis/cibercom/midir/
+git add .
+git commit -m "act ip"
+git push origin main
+cd /home/luis/
+ 
