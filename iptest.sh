7d240d1878883ee87f27b63103f6a57ab26f0358
sistelca <sistelca@hotmail.com>
2021-07-10 12:15:02 -0400

    act ip

--- iptest.sh
+++ iptest.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-/sbin/ip a l enp0s7 | grep "inet " > /home/luis/cibercom/midir/ip.txt
-cd /home/luis/cibercom/midir/
-git add .
-git commit -m "act ip"
-git push origin main
-cd /home/luis/
- 
+VAR1=`/sbin/ip a l enp0s7 | grep "inet "`
+
+input="/home/luis/cibercom/midir/ip.txt"
+
+VAR2=`/bin/cat $input`
+
+#echo "xd"
+#echo $VAR1
+#echo $VAR2
+
+if [ "$VAR1" != "$VAR2" ]; then
+    echo "$VAR1" > "/home/luis/cibercom/midir/ip.txt"
+    /bin/git -C /home/luis/cibercom/midir/ add .
+    /bin/git -C /home/luis/cibercom/midir/ commit -m "act ip"
+    /bin/git -C /home/luis/cibercom/midir/ push origin main
+fi
