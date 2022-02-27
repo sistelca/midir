b0a8673fb5f2a105a9b3e581e7424e7aa81ba52f
sistelca <sistelca@hotmail.com>
2022-02-27 12:00:02 -0400

    act ip

--- iptest.sh
+++ iptest.sh
@@ -12,7 +12,11 @@ VAR2=`/bin/cat $input`
 
 if [ "$VAR1" != "$VAR2" ]; then
     echo "$VAR1" > "/home/luis/cibercom/midir/ip.txt"
+fi
+
+if [ -n "$VAR1" ]; then
     /bin/git -C /home/luis/cibercom/midir/ add .
     /bin/git -C /home/luis/cibercom/midir/ commit -m "act ip"
     /bin/git -C /home/luis/cibercom/midir/ push origin main
 fi
+
