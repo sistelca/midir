e309409a3ec302bca1a70a793cf4cec01e77b6fa
root <root@www.sistelca.com.ve>
2022-04-23 12:06:02 -0400

    act ip

--- iptest.sh
+++ iptest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VAR1=`/sbin/ip a l enp0s7 | grep "inet "`
+VAR1=`/sbin/ip a l enp1s8 | grep "inet "`
 
 input="/home/luis/cibercom/midir/ip.txt"
 
@@ -15,8 +15,8 @@ if [ "$VAR1" != "$VAR2" ]; then
 fi
 
 if [ -n "$VAR1" ]; then
-    /bin/git -C /home/luis/cibercom/midir/ add .
-    /bin/git -C /home/luis/cibercom/midir/ commit -m "act ip"
-    /bin/git -C /home/luis/cibercom/midir/ push origin main
+    /bin/git -C /home/luis/midir/ add .
+    /bin/git -C /home/luis/midir/ commit -m "act ip"
+    /bin/git -C /home/luis/midir/ push origin main
 fi
 
